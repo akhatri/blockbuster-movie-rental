db2878973f8ed07bf5f84f5e572453b3cbdc1b55
Akshat Khatri <akshatster@gmail.com>
2020-05-04 17:31:41 +0100

    setup express server
    - establish mongoDB connection
    - gitignore env files

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+// Imports
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// Initialize App
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Start server
+app.listen(port, ()=> {
+  console.log(`Server is listening on port: ${port}`);
+});
+
+// Server URI
+const uri = process.env.ATLAS_URI;
+
+// Mongoose Parameters
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+// Mongoose Connection
+const connection = mongoose.connection;
+connection.once('open', ()=> {
+  console.log(`MongoDB database connection established successfully`);
+});
+
+// Setup Routes
+
+
