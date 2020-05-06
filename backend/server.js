fa20e6f94535cc5045ad9af44ef917d09fa18368
Akshat Khatri <akshatster@gmail.com>
2020-05-06 19:19:59 +0100

    add bodyparser for larger payload
    - required for image upload of posters

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
@@ -10,7 +11,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 // Start Server
 app.listen(port, ()=> {
