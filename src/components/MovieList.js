c937a60edddaa876db19f5e332245f8ffbd23b63
Akshat Khatri <akshatster@gmail.com>
2020-05-13 20:26:40 +0100

    add filter search to movies

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,11 +11,14 @@ class MovieList extends Component {
     super(props);
 
     this.state = {
-      movies: []
+      movies: [],
+      filteredMovies: [],
+      isLoading: true
     }
 
     // Event binding
     this.deleteMovie = this.deleteMovie.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
 
   }
 
@@ -27,10 +30,11 @@ class MovieList extends Component {
     // Get movie list from Server
     Axios.get('http://localhost:5000/movies')
       .then((res) => {
-        console.log(res.data);
 
         this.setState({
-          movies: res.data
+          movies: res.data,
+          filteredMovies: res.data,
+          isLoading: false
         })
 
       })
@@ -39,6 +43,28 @@ class MovieList extends Component {
 
   // Click Events
   //--------------
+
+  handleSearch(e) {
+
+    let searchTerm = e.target.value;
+
+
+    let filteredMovies = this.state.movies.filter((movie) => {
+
+      let movieName = movie.title.toLowerCase();
+      return movieName.includes(searchTerm);
+
+    });
+
+
+    this.setState({
+      filteredMovies
+    })
+
+    console.log(this.state.movies);
+
+
+  }
 
   deleteMovie = (id) => (e) => {
 
@@ -50,61 +76,85 @@ class MovieList extends Component {
 
   }
 
+  LoadingContainer() {
+
+    return (
+      <div>Loading...</div>
+    )
+
+  }
+
   render() {
+
     return (
       <div>
         <h2 className="display-4 mb-5">Movie Catalogue</h2>
-        <div className="row row-cols-1 row-cols-md-2">
 
-          {
-            this.state.movies.map((movie) => {
 
-              const { title, synopsis, poster, genres } = movie;
+        <div className="form-group mb-5">
+          <input type="search" className="form-control form-control-lg" placeholder="Search Catalogue..." onChange={this.handleSearch} />
+        </div>
 
-              return (
-                <div key={movie._id} className="col mb-4">
-                  <div className="card h-100">
-                    <div className="card-body">
-                      <div className="row">
-                        <div className="col-4">
-                          <img src={poster} className="img-fluid rounded" alt="Movie poster" />
-                        </div>
-                        <div className="col-8">
-                          <h5 className="card-title">{title}</h5>
-                          <p className="card-text">{synopsis}</p>
-                          <div className="mb-2">
-                            {
-                              genres.map((genre, index, array) => {
 
-                                // Add margin to items except last item
-                                let marginClass;
-                                if (array.length - 1 !== index) {
-                                  marginClass = 'mr-2';
-                                } else {
-                                  marginClass = '';
-                                }
+        {
+          this.state.isLoading ? <this.LoadingContainer /> :
+            (
+              <div className="row row-cols-1 row-cols-md-2">
 
-                                return (
-                                  <span key={index} className={`badge badge-primary ${marginClass}`}>{genre}</span>
-                                )
+                {
 
-                              })
-                            }
+                  this.state.filteredMovies.length > 0 ? this.state.filteredMovies.map((movie) => {
+
+                    const { title, synopsis, poster, genres } = movie;
+
+                    return (
+                      <div key={movie._id} className="col mb-4">
+                        <div className="card h-100">
+                          <div className="card-body">
+                            <div className="row">
+                              <div className="col-4">
+                                <img src={poster} className="img-fluid rounded" alt="Movie poster" />
+                              </div>
+                              <div className="col-8">
+                                <h5 className="card-title">{title}</h5>
+                                <p className="card-text">{synopsis}</p>
+                                <div className="mb-2">
+                                  {
+                                    genres.map((genre, index, array) => {
+
+                                      // Add margin to items except last item
+                                      let marginClass;
+                                      if (array.length - 1 !== index) {
+                                        marginClass = 'mr-2';
+                                      } else {
+                                        marginClass = '';
+                                      }
+
+                                      return (
+                                        <span key={index} className={`badge badge-primary ${marginClass}`}>{genre}</span>
+                                      )
+
+                                    })
+                                  }
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="card-footer">
+                            <Link className="btn btn-sm btn-warning mr-2" to={`/edit-movie/${movie._id}`}>Edit</Link>
+                            <button className="btn btn-sm btn-danger" onClick={this.deleteMovie(movie._id)}>Delete</button>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="card-footer">
-                      <Link className="btn btn-sm btn-warning mr-2" to={`/edit-movie/${movie._id}`}>Edit</Link>
-                      <button className="btn btn-sm btn-danger" onClick={this.deleteMovie(movie._id)}>Delete</button>
-                    </div>
-                  </div>
-                </div>
-              )
-            })
-          }
+                    )
+                  }) : (
+                      <p>No movies match your criteria</p>
+                    )
+                }
 
-        </div>
+              </div>
+            )
+        }
       </div>
     )
   }
